9e30196a3f8fb8033f103283ed3faa0e107c088e
kompody <iurii.pavlovsky@mail.ru>
2025-04-27 20:41:01 +0200

    0.1.4

--- src/tm_refs/tm_ref.py
+++ src/tm_refs/tm_ref.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 def krakatit() -> pd.DataFrame:
-    file_path = os.path.join(os.path.dirname(__file__), 'tm_refs', "tm_krakatit.csv")
+    file_path = os.path.join(os.path.dirname(__file__), "tm_krakatit.csv")
     return pd.read_csv(file_path, header=None)
 
 
