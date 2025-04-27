bb8e20db161a084d9ea80c0625fbde03a9a2b3cd
kompody <iurii.pavlovsky@mail.ru>
2025-04-27 20:28:30 +0200

    0.1.4

--- src/tm_refs/tm_ref.py
+++ src/tm_refs/tm_ref.py
@@ -1,14 +1,20 @@
+import os
 import numpy as np
 import pandas as pd
 
+
 def krakatit() -> pd.DataFrame:
-    return pd.read_csv("tm_krakatit.csv", header=None)
+    path = os.path.join(os.path.dirname(__file__), "tm_krakatit.csv")
+    return pd.read_csv(path, header=None)
+
 
 def save_bigrams(bigrams: pd.DataFrame, path: str):
     pd.DataFrame(bigrams).to_csv(path, header=False, index=False)
 
+
 def save_bigrams(bigrams: np.ndarray, path: str):
     pd.DataFrame(bigrams).to_csv(path, header=False, index=False)
+
 
 def load_bigrams(path: str) -> pd.DataFrame:
     return pd.read_csv(path, header=None)
