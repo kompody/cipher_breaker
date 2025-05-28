1971c0f14f64e1909693eafbf5e20d78c18897f9
kompody <iurii.pavlovsky@mail.ru>
2025-05-28 13:40:59 +0200

    0.2.3

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from setuptools import setup, find_packages
 
 requirements = [
     'numpy',
-    'pandas',
     'matplotlib',
 ]
 
@@ -13,7 +12,7 @@ test_requirements = [
 
 setup(
     name='cipher_breaker',
-    version='0.2.2',
+    version='0.2.3',
     packages=find_packages(where='src'),
     package_dir={"": "src"},
     include_package_data=True,
--- src/tm_refs/tm_ref.py
+++ src/tm_refs/tm_ref.py
@@ -1,12 +1,11 @@
 import os
-import pandas as pd
 import numpy as np
 
 def get_krakatit_text() -> str:
     """Load the text of Krakatit from a text file.
 
     Returns:
-        str: A string containing the preprocessed text of Krakatit (e.g., uppercased, non-alphabetic characters removed, and underscores for spaces).
+        str: A string containing the text of Krakatit.
     """
     file_path = os.path.join(os.path.dirname(__file__), "krakatit.txt")
     with open(file_path, "r") as file:
@@ -18,7 +17,8 @@ def get_krakatit_2d_tm() -> np.ndarray:
     Returns:
         np.ndarray: A NumPy array containing the 2D transition matrix.
     """
-    return np.load("krakatit_2d.npy")
+    file_path = os.path.join(os.path.dirname(__file__), "krakatit_2d.npy")
+    return np.load(file_path)
 
 def get_krakatit_3d_tm() -> np.ndarray:
     """Load the 3D transition matrix of Krakatit from a NumPy .npy file.
@@ -26,7 +26,8 @@ def get_krakatit_3d_tm() -> np.ndarray:
     Returns:
         np.ndarray: A NumPy array containing the 3D transition matrix.
     """
-    return np.load("krakatit_3d.npy")
+    file_path = os.path.join(os.path.dirname(__file__), "krakatit_3d.npy")
+    return np.load(file_path)
 
 def get_krakatit_4d_tm() -> np.ndarray:
     """Load the 4D transition matrix of Krakatit from a NumPy .npy file.
@@ -34,5 +35,6 @@ def get_krakatit_4d_tm() -> np.ndarray:
     Returns:
         np.ndarray: A NumPy array containing the 4D transition matrix.
     """
-    return np.load("krakatit_4d.npy")
+    file_path = os.path.join(os.path.dirname(__file__), "krakatit_4d.npy")
+    return np.load(file_path)
 
