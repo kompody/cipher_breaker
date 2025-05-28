a18c9d3ba834dc8b07b59444f30994b15f9b58a4
kompody <iurii.pavlovsky@mail.ru>
2025-05-28 13:47:29 +0200

    0.2.4

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ test_requirements = [
 
 setup(
     name='cipher_breaker',
-    version='0.2.3',
+    version='0.2.4',
     packages=find_packages(where='src'),
     package_dir={"": "src"},
     include_package_data=True,
