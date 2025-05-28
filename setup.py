aada91d10e8dc8ff770957863f06d9f9efa30a3f
kompody <iurii.pavlovsky@mail.ru>
2025-05-28 13:37:52 +0200

    0.2.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ test_requirements = [
 
 setup(
     name='cipher_breaker',
-    version='0.2.1',
+    version='0.2.2',
     packages=find_packages(where='src'),
     package_dir={"": "src"},
     include_package_data=True,
--- src/tm_refs/__init__.py
+++ src/tm_refs/__init__.py
@@ -1,3 +1,3 @@
-from .tm_ref import krakatit, get_krakatit_text, get_krakatit_2d_tm, get_krakatit_3d_tm, get_krakatit_4d_tm
+from .tm_ref import get_krakatit_text, get_krakatit_2d_tm, get_krakatit_3d_tm, get_krakatit_4d_tm
 
-__all__ = ["krakatit", "get_krakatit_text", "get_krakatit_2d_tm", "get_krakatit_3d_tm", "get_krakatit_4d_tm"]
+__all__ = ["get_krakatit_text", "get_krakatit_2d_tm", "get_krakatit_3d_tm", "get_krakatit_4d_tm"]
